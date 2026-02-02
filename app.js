9ad97da185dfe95d28b22b9aff6874f1b0771668
davidbedoya <davidbedoya1000@gmail.com>
2026-02-02 18:44:41 -0500

    Clothing Items Portions Completed

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const { PORT = 3001 } = process.env;
+const app = express();
+
+const mainRouter = require("./routes/index");
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch(console.error);
+
+app.use(express.json());
+app.use("/", mainRouter);
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
---
+++ controllers/clothingItem.js
@@ -0,0 +1,48 @@
+const ClothingItem = require("../models/clothingItems");
+
+const createItem = (req, res) => {
+  console.log(req);
+  console.log(req.body);
+
+  const { name, weather, imageURL } = req.body;
+  ClothingItem.create({ name, weather, imageURL })
+    .then((item) => {
+      console.log(item);
+      res.send({ data: item });
+    })
+    .catch((e) => {
+      res.status(500).send({ message: "Error from createItem " });
+    });
+};
+
+const getItems = (req, res) => {
+  ClothingItem.find({})
+    .then((items) => res.status(200).send(items))
+    .catch((e) => {
+      // console.error;
+      res.status(500).send({ message: "Get Items Failed ", e });
+    });
+};
+
+const updateItem = (req, res) => {
+  const { itemId } = req.params;
+  const { imageURL } = req.body;
+
+  ClothingItem.findByIdAndUpdate(itemId, { $set: { imageURL } })
+    .orFail()
+    .then((item) => res.status(200).send({ data: item }))
+    .catch((e) => {
+      res.status(500).send({ message: "Error from getItems", e });
+    });
+};
+
+const deleteItem = (req, res) => {
+  const { itemId } = req.params;
+  ClothingItem.findByIdAndDelete(itemId)
+    .orFail()
+    .then((item) => res.status(204).send({}))
+    .catch((e) => {
+      res.status(500).send({ message: "Error from getItems", e });
+    });
+};
+module.exports = { createItem, getItems, updateItem, deleteItem };
