288e65e7bdd605dc82a39909e3cebc7f49fd3a8e
davidbedoya <davidbedoya1000@gmail.com>
2026-03-02 13:09:59 -0500

    project 14 backend submission

--- models/user.js
+++ models/user.js
@@ -6,9 +6,11 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true, minlength: 2, maxlength: 30 },
   avatar: {
     type: String,
-    required: [true, "The avatar field is required."],
+    // required: [true, "The avatar field is required."],
+    default: "",
     validate: {
       validator(value) {
+        if (!value) return true; // allow missing or empty
         return validator.isURL(value);
       },
     },
